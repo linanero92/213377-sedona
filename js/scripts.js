fe941a9b77908ec7524d16717ea55baf0de3fb62
linanero92 <31183099+linanero92@users.noreply.github.com>
2017-12-06 00:36:12 +0300

    создание js отктие-закрытие формы, валидация, анимация, и минифицированный js

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var modalButton = document.querySelector(".search-button");
+var modal = document.querySelector(".select");
+var arrivalDate = modal.querySelector("[name=arrival]");
+var departureDate = modal.querySelector("[name=departure]");
+var adultsAmount = modal.querySelector("[name=adults]");
+var childrenAmount = modal.querySelector("[name=children]");
+var form = document.querySelector("form");
+
+modalButton.addEventListener("click", function(){
+  modal.classList.toggle("form-hide")
+});
+
+
+function postValidation() {
+  alert("Необходимо ввести количество взрослых и/или детей")
+  modal.classList.remove("form-error");
+}
+
+function formValidate() {
+  if ((!adultsAmount.value && !childrenAmount.value) || 
+    (adultsAmount.value == "0" && childrenAmount.value == "0") || 
+    (adultsAmount.value == "0" && !childrenAmount.value) || 
+    (!adultsAmount.value && childrenAmount.value == "0")||
+     (adultsAmount.value == 0 && childrenAmount.value != "0") ) {
+     modal.classList.add("form-error");
+     modal.offsetWidth = modal.offsetWidth;
+     setTimeout(postValidation, 1000);
+   
+  return false
+ } 
+}
